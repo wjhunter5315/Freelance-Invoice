11daf8556bb288e0a1f76a4472842c3f070d3d5c
wjhunter5315 <wjhunter5315@gmail.com>
2019-12-06 14:11:28 -0800

    comments added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,36 +1,48 @@
 const db = require("../models");
 
 module.exports = (app => {
+    //Get all users
     app.get("/api/user", (req, res) => {
         db.User.findAll({}).then(dbUser => {
         res.json(dbUser);
         });
     });
-
+    //Get one user by id
+    app.get("/api/user/:id", (req, res) => {
+        db.User.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.User]
+        }).then(dbUser => {
+            res.json(dbUser);
+        });
+    });
+    //Get the user's education
     app.get("/api/userEDU", (req, res) => {
         db.UserEDU.findAll({}).then(dbuserEDU => {
             res.json(dbuserEDU);
         });
     });
-
+    //Get the user's experience
     app.get("/api/userExp", (req, res) => {
         db.UserEpx.findAll({}).then(dbuserExp => {
             res.json(dbuserExp);
         });
     });
-
+    //Get the services the user has applied for
     app.get("/api/services", (req, res) => {
         db.Services.findAll({}).then(dbServices => {
             res.json(dbServices);
         });
     });
-    
+    //Get the invoices the user has created
     app.get("/api/invoices", (req, res) => {
         db.Invoices.findAll({}).then(dbInvoices => {
             res.json(dbInvoices);
         });
     });
-
+    //Create a new user
     app.post("/api/user", (req, res) => {
         db.User.create({
             firstName: req.body.name,
@@ -45,7 +57,7 @@ module.exports = (app => {
             res.json(dbUser);
         });
     });
-
+    //Create a new services page for the user
     app.post("/api/services", (req, res) => {
         db.Services.create({
             labor: req.body.labor,
@@ -54,7 +66,7 @@ module.exports = (app => {
             res.json(dbServices);
         });
     });
-
+    //Create an invoice for the service taken
     app.post("/api/invoices", (req, res) => {
         db.Invoices.create({
             client: req.body.client
@@ -62,7 +74,7 @@ module.exports = (app => {
             res.json(dbInvoices);
         });
     });
-
+    //Create an education object for the user
     app.post("/api/education", (req, res) => {
         db.UserEDU.create({
             level: req.body.level,
@@ -73,7 +85,7 @@ module.exports = (app => {
             res.json(dbuserEDU);
         });
     });
-
+    //Create an experience object for the user
     app.post("/api/experience", (req, res) => {
         db.UserExp.create({
             prevJob: req.body.prevJob,
@@ -83,7 +95,7 @@ module.exports = (app => {
             res.json(dbuserExp);
         });
     });
-
+    //Delete a user by the id
     app.delete("/api/user/:id", (req, res) => {
         db.User.destroy({
             where: {
@@ -93,7 +105,7 @@ module.exports = (app => {
             res.json(dbUser);
         });
     });
-
+    //Delete education from a user by id
     app.delete("/api/userEDU/:id", (req, res) => {
         db.UserEDU.destroy({
             where: {
@@ -103,7 +115,7 @@ module.exports = (app => {
             res.json(dbuserEDU);
         });
     });
-
+    //Delete work experience from a user by id
     app.delete("/api/userExp/:id", (req, res) => {
         db.UserEpx.destroy({
             where: {
@@ -113,7 +125,7 @@ module.exports = (app => {
             res.json(dbuserExp);
         });
     });
-
+    //Delete a service from the user by id
     app.delete("/api/servives/:id", (req, res) => {
         db.Services.destroy({
             where: {
@@ -123,7 +135,7 @@ module.exports = (app => {
             res.json(dbServices);
         });
     });
-
+    //Delete an invoice for a service for a user by id
     app.delete("/api/invoices/:id", (req, res) => {
         db.Invoices.destroy({
             where: {

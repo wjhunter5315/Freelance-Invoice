2374e0fd83fc5f178f021998ab89a1ba89dc00f4
David Pang <dpang2@uci.edu>
2019-12-05 19:23:23 -0800

    database update

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,33 @@
 DROP DATABASE IF EXISTS freelance_db;
 CREATE DATABASE freelance_db;
 USE freelance_db;
+
 CREATE TABLE users_info (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(100) NOT NULL,
-    lastname VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    location varchar(100) NOT NULL,
+    password varchar(50) NOT NULL,
+    school varchar (100) NOT NULL,
+    degree_certificate varchar (50) NOT NULL,
+    fieldofstudy varchar (50) NOT NULL
 );
-CREATE TABLE profile_info (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    userID INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(userID) REFERENCES users_info(id)
-);
+
+CREATE TABLE users_experience (
+    userID INTEGER PRIMARY KEY,
+    title varchar (50),
+    company varchar (50),
+    jobdescrption varchar (50)
+    userexpID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY(userexpID) REFERENCES users_info(id)
+
+)
+
+
+
+
 
 
 -- Push in damn you
+
+

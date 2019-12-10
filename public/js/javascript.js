42de62600624e57828e86fc0dc01c581dcbf34d5
Glenny <55863951+anthonychry@users.noreply.github.com>
2019-12-10 14:09:30 -0800

    consolidated api-routes & javascript

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,3 +1,5 @@
+//generic profile maker
+
 $("#continue-button").on("click", function (event) {
 event.preventDefault();
 
@@ -28,3 +30,47 @@ event.preventDefault();
     })
   }
 });
+
+//education button
+
+$("#continue-button").on("click", function(event) {
+  event.preventDefault();
+  
+  var UserEdu = {
+      school: $("#school-input").val(),
+      degree: $("#degree-input").val(),
+      field: $("#field-input").val()
+  };
+    
+  $.ajax({
+      method: "POST",
+      url: "/api/UserEdu",
+      data: UserEdu
+    }).then((result) => console.log(result))
+    .catch((err) => {
+      if (err) throw err;
+    })
+  
+  });
+
+//experience info
+
+$("#upload-button").on("click", function() {
+    console.log("Button is working.");
+});
+
+$("#continue-button").on("click", function() {
+  var UserExp = {
+      title: $("#title-input").val(),
+      company: $("#company-input").val(),
+      description: $("#job-description-input").val()
+  }
+
+  $.ajax({
+    method: "POST",
+    url: "/api/UserExp",
+    data: UserExp
+  }).catch((err) => {
+    if (err) throw err;
+  })
+  });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,18 +7,6 @@ module.exports = (app => {
         });
     });
 
-    app.get("/api/UserEDU", (req, res) => {
-        db.UserEdu.findAll({}).then(dbuserEDU => {
-            res.json(dbuserEDU);
-        });
-    });
-
-    app.get("/api/UserExp", (req, res) => {
-        db.UserExp.findAll({}).then(dbuserExp => {
-            res.json(dbuserExp);
-        });
-    });
-
     app.get("/api/services", (req, res) => {
         db.Services.findAll({}).then(dbServices => {
             res.json(dbServices);
@@ -40,10 +28,15 @@ module.exports = (app => {
             userEmail: req.body.userEmail,
             userPhone: req.body.userPhone,
             username: req.body.username,
-            password: req.body.password
+            password: req.body.password,
+            school: req.body.school,
+            degree: req.body.degree,
+            field: req.body.field,
+            title: req.body.title,
+            company: req.body.company,
+            description: req.body.description
         }).then(dbUser => {
-            console.log(dbUser);
-            // res.json(dbUser);
+            res.json(dbUser);
         });
     });
 
@@ -64,29 +57,6 @@ module.exports = (app => {
     //     });
     // });
 
-    app.post("/api/UserEdu", (req, res) => {
-        db.UserEdu.create({
-            school: req.body.school,
-            degree: req.body.degree,
-            field: req.body.field
-        }).then(dbuserEDU => {
-            const user = dbuserEDU.getUser();
-            let userProfile = { user, dbuserEDU }
-
-            res.json(userProfile);
-        });
-    });
-
-    app.post("/api/UserExp", (req, res) => {
-        db.UserExp.create({
-            title: req.body.title,
-            company: req.body.company,
-            description: req.body.description
-        }).then(dbuserExp => {
-            res.json(dbuserExp);
-        });
-    });
-
     // app.put("/api/User", (req, res) => {
     //     db.User.update({
     //         firstName: req.body.name,
@@ -96,33 +66,10 @@ module.exports = (app => {
     //         userEmail: req.body.email,
     //         userPhone: req.body.email,
     //         username: req.body.userName,
-    //         password: req.body.password
-    //     }, {
-    //         where: {
-    //             id: req.body.id
-    //         }
-    //     }).then(dbUser => {
-    //         res.json(dbUser);
-    //     });
-    // });
-
-    // app.put("/api/education", (req, res) => {
-    //     db.UserEDU.update({
-    //         level: req.body.level,
+    //         password: req.body.password,
     //         school: req.body.school,
     //         degree: req.body.degree,
-    //         field: req.body.field
-    //     }, {
-    //         where: {
-    //             id: req.body.id
-    //         }
-    //     }).then(dbUserEDU => {
-    //         res.json(dbUserEDU);
-    //     });
-    // });
-
-    // app.put("/api/experience", (req, res) => {
-    //     db.userExp.update({
+    //         field: req.body.field,
     //         title: req.body.title,
     //         company: req.body.company,
     //         description: req.body.description
@@ -130,8 +77,8 @@ module.exports = (app => {
     //         where: {
     //             id: req.body.id
     //         }
-    //     }).then(dbuserExp => {
-    //         res.json(dbuserExp);
+    //     }).then(dbUser => {
+    //         res.json(dbUser);
     //     });
     // });
 
@@ -167,26 +114,6 @@ module.exports = (app => {
 //             }
 //         }).then(dbUser => {
 //             res.json(dbUser);
-//         });
-//     });
-
-//     app.delete("/api/userEDU/:id", (req, res) => {
-//         db.UserEDU.destroy({
-//             where: {
-//                 id: req.params.id
-//             }
-//         }).then(dbuserEDU => {
-//             res.json(dbuserEDU);
-//         });
-//     });
-
-//     app.delete("/api/userExp/:id", (req, res) => {
-//         db.userExp.destroy({
-//             where: {
-//                 id: req.params.id
-//             }
-//         }).then(dbuserExp => {
-//             res.json(dbuserExp);
 //         });
 //     });
 

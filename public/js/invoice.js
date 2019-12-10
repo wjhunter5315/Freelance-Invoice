76595875c7613d84d22517a8b43cf64fc16a8783
wjhunter5315 <wjhunter5315@gmail.com>
2019-12-10 15:10:00 -0800

    working on making invoices

---
+++ public/js/invoice.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#make-invoice").on("click", function() {
+        makeInvoice()
+    });
+    function makeInvoice() {
+        let invoiceNumber = document.getElementById("#invoice-number").nodeValue;
+        let invoiceDesc = document.getElementById("#invoice-description").nodeValue;
+        let invoiceRate = document.getElementById("#invoice-rate").nodeValue;
+
+        
+    }
+});

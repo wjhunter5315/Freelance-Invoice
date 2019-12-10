6bd03d7d6281fc079fb253443cab7827249af0fa
wjhunter5315 <wjhunter5315@gmail.com>
2019-12-10 13:54:49 -0800

    fixrd user model and profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,33 +1,16 @@
 //NAV BAR ON-CLICK FUNCTIONS
 $(document).ready(function() {
-    function getName () {
+    function getInfo () {
         $.get("/api/User", function(data) {
             let fullName = [];
+            let userId = data[0].id;
             console.log(data);
             console.log(data[0].firstName);
-            fullName = `${data[0].firstName} ${data[0].lastName}`
+            fullName = `${data[0].firstName} ${data[0].lastName} ID:${userId}`
             console.log(fullName);
-            // $("#full-name-profile").val(fullName);
-            $("#full-name-profile").append(fullName);
-        });
-    }
-    getName();
-
-    function getEducation () {
-        $.get("/api/UserEDU", function(data) {
             let schoolName = data[0].school;
             let degreeGot = data[0].degree;
             let fieldDeg = data[0].field;
-            console.log(schoolName, degreeGot, fieldDeg);
-            $("#school-info").append(schoolName);
-            $("#degree-info").append(degreeGot);
-            $("#field-info").append(fieldDeg);
-        });
-    }
-    getEducation();
-
-    function getExperience () {
-        $.get("/api/UserExp", function(data) {
             let expTitle = data[0].title;
             let expCompany = data[0].company;
             let expDescription = data[0].description;
@@ -35,10 +18,42 @@ $(document).ready(function() {
             $("#company-info").append(expCompany);
             $("#title-info").append(expTitle);
             $("#description-info").append(expDescription);
+            $("#job-title").append(expTitle);
+            $("#full-name-profile").append(fullName);
+            $("#school-info").append(schoolName);
+            $("#degree-info").append(degreeGot);
+            $("#field-info").append(fieldDeg);
         });
     }
-    getExperience();
-})
+    getInfo();
+
+//     function getEducation () {
+//         $.get("/api/UserEDU", function(data) {
+//             let schoolName = data[0].school;
+//             let degreeGot = data[0].degree;
+//             let fieldDeg = data[0].field;
+//             console.log(schoolName, degreeGot, fieldDeg);
+//             $("#school-info").append(schoolName);
+//             $("#degree-info").append(degreeGot);
+//             $("#field-info").append(fieldDeg);
+//         });
+//     }
+//     getEducation();
+
+//     function getExperience () {
+//         $.get("/api/UserExp", function(data) {
+//             let expTitle = data[0].title;
+//             let expCompany = data[0].company;
+//             let expDescription = data[0].description;
+//             console.log(expTitle, expCompany, expDescription);
+//             $("#company-info").append(expCompany);
+//             $("#title-info").append(expTitle);
+//             $("#description-info").append(expDescription);
+//             $("#job-title").append(expTitle);
+//         });
+//     }
+//     getExperience();
+// })
 
 //Home nav button
 $("#home-button-nav").on("click", function() {

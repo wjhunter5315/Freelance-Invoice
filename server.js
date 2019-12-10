bf596cfb1ac6426a25a7df8610c0e55dd7092852
Reggie Alleyne <reggiealleyne89@gmail.com>
2019-12-10 12:52:09 -0800

    changes on server.js (multer) and html-routes

--- server.js
+++ server.js
@@ -11,29 +11,64 @@ app.use(express.json());
 // Static directory to be served
 app.use(express.static("public"));
 
-// // require Multer npm
-// const multer = require('multer');
+// require Multer npm
+const multer = require('multer');
 
-// // Storing multer files
-// var storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'uploads')
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.fieldname + '-' + Date.now())
-//   }
-// })
+// Storing multer files
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets/img')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
  
-// var upload = multer({ storage: storage })
+var upload = multer({ storage: storage })
 
-// // Multer routes
-// app.get('/experience',(req,res) => {
-//   res.render('experience',  {title: 'upload file'});
-// });
+// Multer routes
+app.get('/experience',(req,res) => {
+  res.render('experience',  {title: 'upload file'});
+});
 
-// app.post('/upload', multer(storage).single('resume'), (req, res) => {
-//   res.json('This is post route upload')
-// })
+// creating a post for a single upload
+app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    res.send(file)
+  
+})
+
+// retrieving the stored upload
+app.get('/resume', (req, res) => {
+  db.collection('mycollection').find().toArray((err, result) => {
+   
+        const resume= result.map(element => element._id);
+              console.log(resume);
+   
+     if (err) return console.log(err)
+     res.send(resume)
+   
+    })
+  });
+
+  app.get('/photo/:id', (req, res) => {
+    var filename = req.params.id;
+     
+    db.collection('mycollection').findOne({'_id': ObjectId(filename) }, (err, result) => {
+     
+        if (err) return console.log(err)
+     
+       res.contentType('image/jpeg');
+       res.send(result.image.buffer)
+       
+        
+      })
+    })
 
 // api & html routes
 require("./routes/api-routes.js")(app);
